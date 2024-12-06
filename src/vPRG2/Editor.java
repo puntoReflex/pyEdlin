3d41c817ebfc111ef83dd890d1195bd9cff640e4
manuel@sdf1 <manuel@masiasweb.com>
2024-12-06 17:53:36 +0100

    Rediseño persistencia en Editor
    - Constructor único para inicialización básica
    - setStorage para establecer archivo destino
    - hasStorage para verificar estado

--- src/vPRG2/Editor.java
+++ src/vPRG2/Editor.java
@@ -3,12 +3,23 @@ package vPRG2;
 public class Editor {
     private Document document;
     private UserInterface ui;
-    
+    private DocumentSerializer serializer;
+    private DocumentStorage storage;
+
     public Editor() {
+        this.serializer = new SimpleSerializer();
         this.document = new Document();
         this.ui = new UserInterface();
     }
-    
+
+    public void setStorage(String filePath) {
+        this.storage = new FileManagerAdapter(filePath);
+    }
+
+    public boolean hasStorage() {
+        return storage != null;
+    }
+
     public void run() {
         boolean running = true;
         while (running) {
@@ -17,9 +28,9 @@ public class Editor {
             running = command.execute(document);
         }
     }
-    
+
     public static void main(String[] args) {
         Editor editor = new Editor();
         editor.run();
     }
- }
+}
